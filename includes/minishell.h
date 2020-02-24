ff67f34be661fd73803b29f6cfd0361ad62baf74
Antoine LE BAUX <antoine.stc25@gmail.com>
2020-02-24 11:49:24 +0100

    command chaned list structure

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: ale-baux <ale-baux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/24 10:08:19 by ale-baux          #+#    #+#             */
-/*   Updated: 2020/02/24 10:35:04 by ale-baux         ###   ########.fr       */
+/*   Updated: 2020/02/24 11:49:09 by ale-baux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,16 @@
 #include <libc.h>
 #include "../libft/libft.h"
 #include "./command.h"
+
+
+typedef struct	s_command
+{
+	int cmd;
+	char flag;
+	char *arg;
+	int value_returned;
+	t_command *next;
+	t_command *prev;
+}				t_command
 
 #endif
