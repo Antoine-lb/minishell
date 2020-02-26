6c6002dbfb33818edd335ae773ae3469279eb2f1
Antoine LE BAUX <antoine.stc25@gmail.com>
2020-02-26 16:28:42 +0100

    (not done) execution of the commands

--- sources/main.c
+++ sources/main.c
@@ -5,6 +5,36 @@ void	fdf(void *cmd)
 	printf("cmd->cmd = %s\n", ((t_command*)cmd)->cmd);
 }
 
+int execute_command(t_list *cmd_line)
+{
+	int status;
+	pid_t pid_fils;
+	t_command *content;
+
+	if (!cmd_line)
+		cmd_line = NULL;
+	pid_fils = fork();
+	while (cmd_line)
+	{
+		if (pid_fils == 0)
+		{
+			content = cmd_line->content;
+			printf("content->cmd = %s\n", content->cmd);
+			execl ("/bin/ls", NULL) ;
+
+			exit(0);
+		}
+		else {
+			wait(&status);
+			pid_fils = fork();
+		}
+		cmd_line = cmd_line->next;
+	}
+	wait(&status);
+
+	return (0);
+}
+
 int rep(void)
 {
 	int ret;
@@ -29,6 +59,7 @@ int rep(void)
 	parse(cmd_text, cmd_tmp);
 	ft_lstadd_back(&cmd, ft_lstnew(cmd_tmp));
 	ft_lstiter(cmd, fdf);
+	execute_command(cmd);
 	return (ret);
 }
 
