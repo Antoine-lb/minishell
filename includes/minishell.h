979b446871d253b4675190787e53674e90b747e5
Antoine LE BAUX <antoine.stc25@gmail.com>
2020-03-11 09:24:33 +0100

    error when command not fund and move execute command to other file

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: ale-baux <ale-baux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/24 10:08:19 by ale-baux          #+#    #+#             */
-/*   Updated: 2020/03/11 09:06:53 by ale-baux         ###   ########.fr       */
+/*   Updated: 2020/03/11 09:15:20 by ale-baux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,6 @@
 char *get_path_from_env(char *cmd);
 int *print_promt(void);
 void handle_sig(int sig);
+int execute_commands(t_list *cmd_line);
 
 #endif
---
+++ sources/execute_commands.c
@@ -0,0 +1,155 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   execute_commands.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ale-baux <ale-baux@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/03/11 09:13:35 by ale-baux          #+#    #+#             */
+/*   Updated: 2020/03/11 09:22:45 by ale-baux         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+int is_last_symbol(t_list *redirections, int target)
+{
+	int sep;
+
+	redirections = redirections->next;
+	while (redirections)
+	{
+		sep = (int)((t_redirection *)(redirections->content))->sep;
+
+		if (sep == target)
+		{
+			return (0);
+		}
+		redirections = redirections->next;
+	}
+	return (1);
+}
+
+int get_fd_in_and_out(t_command *content, int *fdin, int *fdout)
+{
+	t_list *redirections;
+	int input_has_changed;
+	int sep;
+
+	input_has_changed = 0;
+	redirections = content->redirections;
+	while (redirections)
+	{
+		sep = (int)((t_redirection *)(redirections->content))->sep;
+		if (sep == 3)
+		{
+			*fdin = open(((t_redirection *)(redirections->content))->args, O_RDONLY, 0666);
+			if (!is_last_symbol(redirections, 3))
+			{
+				close(*fdin);
+			}
+		}
+		else if (sep == 4)
+		{
+			*fdout = open(((t_redirection *)(redirections->content))->args, O_RDWR | O_CREAT | O_TRUNC, 0666);
+			if (!is_last_symbol(redirections, 4) || !is_last_symbol(redirections, 5))
+			{
+				close(*fdout);
+			}
+		}
+		else if (sep == 5)
+		{
+			*fdout = open(((t_redirection *)(redirections->content))->args, O_RDWR | O_CREAT | O_APPEND, 0666);
+			if (!is_last_symbol(redirections, 4) || !is_last_symbol(redirections, 5))
+			{
+				close(*fdout);
+			}
+		}
+		redirections = redirections->next;
+	}
+	return (input_has_changed);
+}
+
+int execute_commands(t_list *cmd_line)
+{
+	int status;
+	char **tab;
+	char *tmp;
+	t_command *content;
+
+	if (!cmd_line)
+		cmd_line = NULL;
+	int tmpin = dup(0);
+	int tmpout = dup(1);
+	int fdin = dup(tmpin);
+	int fdout = dup(tmpout);
+	int ret;
+	while (cmd_line)
+	{
+		int infile = 0;
+		int outfile = 0;
+		content = (t_command *)(cmd_line->content);
+		tab = execution(content);
+		dup2(fdin, 0);
+		close(fdin);
+		ret = get_fd_in_and_out(content, &infile, &outfile);
+		if (ret == -1)
+			printf("error reading fdin or fdout\n");
+		if (infile)
+			fdin = infile;
+		if (cmd_line->next == NULL)
+		{
+			if (outfile)
+				fdout = outfile;
+			else
+				fdout = dup(tmpout);
+		}
+		else
+		{
+			int fdpipe[2];
+			pipe(fdpipe);
+			fdin = fdpipe[0];
+			if (outfile)
+			{
+				fdout = outfile;
+				close(fdpipe[1]);
+			}
+			else
+				fdout = fdpipe[1];
+		}
+		dup2(fdout, 1);
+		close(fdout);
+		*print_promt() = 1;
+		if (ft_strlen(tab[0]) != 0)
+		{
+			ret = fork();
+			if (ret == 0)
+			{
+				tmp = get_path_from_env(tab[0]);
+				if (tmp != NULL)
+				{
+					execve(tmp, tab, NULL);
+					perror("execve");
+				}
+				else
+				{
+					ft_putstr_fd("minishell: [", 2);
+					ft_putstr_fd(tab[0], 2);
+					ft_putstr_fd("] command not found\n", 2);
+				}
+
+				free(tmp);
+				exit(0);
+			}
+			else
+				signal(SIGCHLD, SIG_IGN);
+		}
+		cmd_line = cmd_line->next;
+	}
+	wait(&status);
+	dup2(tmpin, 0);
+	dup2(tmpout, 1);
+	close(tmpin);
+	close(tmpout);
+	return (0);
+}
