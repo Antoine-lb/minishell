95ec6a04be57c90b9177bfb97e992042952e94cd
Antoine Le Baux <antoine.stc25@gmail.com>
2020-05-25 11:56:51 +0200

    get_path_from_env norm

--- sources/get_path_from_env.c
+++ sources/get_path_from_env.c
@@ -12,9 +12,9 @@
 
 #include "../includes/minishell.h"
 
-void free_all_splited_paths(char **splited_paths)
+void	free_all_splited_paths(char **splited_paths)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	while (splited_paths[i])
@@ -25,11 +25,11 @@ void free_all_splited_paths(char **splited_paths)
 	free(splited_paths);
 }
 
-char *file_exists(char *cmd, char *path)
+char	*file_exists(char *cmd, char *path)
 {
-	char *tmp;
-	char *tmp_2;
-	int fd;
+	char	*tmp;
+	char	*tmp_2;
+	int		fd;
 
 	tmp = ft_strjoin(path, "/");
 	tmp_2 = ft_strjoin(tmp, cmd);
@@ -45,12 +45,12 @@ char *file_exists(char *cmd, char *path)
 	return (NULL);
 }
 
- char *get_path_from_env(char *cmd, char ***env)
- {
-	int i;
-	char *all_paths;
-	char **splited_paths;
-	char *ret;
+char	*get_path_from_env(char *cmd, char ***env)
+{
+	int		i;
+	char	*all_paths;
+	char	**splited_paths;
+	char	*ret;
 
 	i = 0;
 	if (!cmd || cmd[0] == '.' || cmd[0] == '/')
